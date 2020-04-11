81215cd1f453bd5c7a563b6aaccc5fc23eec0c7c
Song <814810390@qq.com>
2020-04-12 00:12:09 +0800

    完成

--- pat1038/main2.py
+++ pat1038/main2.py
@@ -2,8 +2,8 @@ num_list = sorted(list(map(str, input().split(' ')))[1:])
 string = ''
 while num_list:
     first_num = num_list[0]
-    if len(num_list)>1 and first_num == num_list[1][:len(first_num)]:
-        if num_list[1][len(first_num)] < first_num[0]:
+    if len(num_list)>1:
+        if first_num + num_list[1] > num_list[1] + first_num:
             string += num_list.pop(1)
         else:
             string += num_list.pop(0)
